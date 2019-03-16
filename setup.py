2bc01806a448e374f9fb3cf62f1cfa7ebaadf2f2
Aviad Rom <aviad@zebra-med.com>
2019-03-16 23:07:04 +0200

    adding description and url to setup

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     author="Aviad Rom",
     version="0.1.0",
     packages=find_packages(),
+    description="Enforcing Language Naming Conventions",
+    long_description=open("README.md").read(),
+    url="https://github.com/RomAviad/jpsy",
     classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
