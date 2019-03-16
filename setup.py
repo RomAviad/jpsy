4568bb9a885a16dfbce87c60de6cffbce870f2f2
Aviad Rom <aviad@zebra-med.com>
2019-03-16 23:13:28 +0200

    pypi changes

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name="jpsy",
     author="Aviad Rom",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     description="Enforcing Language Naming Conventions",
     long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/RomAviad/jpsy",
     classifiers=[
         "Programming Language :: Python",
